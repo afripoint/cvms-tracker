d48a3da6c19cc6ac35bb4d40eff6e92690ac80b8
firmprojects <kcvontop@gmail.com>
2024-11-04 11:02:41 +0100

    deployment first commit

--- app/settings.py
+++ app/settings.py
@@ -1,19 +1,13 @@
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure--9!^hok=c8!&hr23in%zzqxd%s9i@8e23m1ek*jhw65a%z4fj$"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -62,12 +56,29 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "cvmstrackerdb",
+        "USER": "afriauthdb_owner",
+        "PASSWORD": "ZfxjP54cLIdz",
+        "HOST": "ep-silent-frost-a2griadk.eu-central-1.aws.neon.tech",
+        "PORT": 5432,
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
+
+# postgresql://afriauthdb_owner:ZfxjP54cLIdz@ep-silent-frost-a2griadk.eu-central-1.aws.neon.tech/cvmstrackerdb?sslmode=require
 
 
 # Password validation
@@ -101,12 +112,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
