5fb4a93d5d1bd0d4a5ed3f60d5bf0b177985ac4c
firmprojects <kcvontop@gmail.com>
2024-12-05 10:58:29 +0100

    added CORS_ALLOW_HEADERS to settings.py

--- app/settings.py
+++ app/settings.py
@@ -140,6 +140,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "x-api-key",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://tracker.cvmsnigeria.com",
